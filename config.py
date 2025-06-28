6e12299269d17e116cfb0ff7de9a81382c04c996
NOBITA <nobitaanetwork@gmail.com>
2025-06-28 22:25:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,11 +7,11 @@ load_dotenv()
 API_ID = int(getenv("API_ID", "21803165"))
 API_HASH = getenv("API_HASH", "05e5e695feb30e25bef47484cc006da7")
 BOT_TOKEN = getenv("BOT_TOKEN", None)
-OWNER_ID = int(getenv("OWNER_ID", "7403621976"))
+OWNER_ID = int(getenv("OWNER_ID", "8150875959"))
 MONGO_URL = getenv("MONGO_URL", None)
-SUPPORT_GRP = getenv("SUPPORT_GRP", "Purvi_UPdates")
-UPDATE_CHNL = getenv("UPDATE_CHNL", "PURVI_SUPPORT")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "ll_ALPHA_BABY_lll")
+SUPPORT_GRP = getenv("SUPPORT_GRP", "AboutBotMaker")
+UPDATE_CHNL = getenv("UPDATE_CHNL", "KING_BOT_UPDATE")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "KING_BOTS_MAKER")
 
 # Random Start Images
 IMG = [
